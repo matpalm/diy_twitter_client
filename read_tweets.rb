4f159fdbd5a68a6490f373b2fc259085d4e72a03
mat <matthew.kelcey@gmail.com>
2010-11-13 14:35:01 +1100

    make read_tweets a while true loop for rating tweets

--- read_tweets.rb
+++ read_tweets.rb
@@ -3,12 +3,46 @@ require 'tweets'
 
 client = Tweets.new
 
-tweets = client.get_latest_unread 10
+while true do
+  tweets = client.get_latest_unread 5
 
-tweets.each do |t|
-  id, text, read  = %w(id text read).map{|k| t[k]}
-  screen_name = t['user']['name']
-  puts [id, read, screen_name, text].join("\t")
+  if tweets.empty?
+    puts "no more left, go crawl some more sucka!"
+    exit 0
+  end
+
+  tweets.each_with_index do |t, idx|
+    id, text, read  = %w(id text read).map{|k| t[k]}
+    screen_name = t['user']['name']
+    puts [idx, id, read, screen_name, text].join("\t")
+  end
+
+  idx = 0
+  print "> "
+  cmd = gets.chomp
+  cmd.chars.to_a.each do |c|
+    case c
+    when 'u'
+      client.mark_thumbs_up tweets[idx]
+      idx += 1
+    when 'd'
+      client.mark_thumbs_down tweets[idx]
+      idx += 1
+    when 'n'
+      client.mark_neutral tweets[idx]
+      idx += 1
+    when 'x'
+      exit 0
+    else 
+      raise "don't know what [#{c}] means, sorry; expected one of [udnx]"
+    end
+
+  end
+
+  puts client.stats.inspect
+
 end
 
-puts client.stats.inspect
+
+
+
--- tweets.rb
+++ tweets.rb
@@ -30,22 +30,39 @@ class Tweets
   end
 
   def get_latest_unread n=10
-    @mongo.find({ :read => false }).limit(n).sort(['id','descending'])
+    @mongo.find({ :thumbs => { "$exists" => false }}).limit(n).sort(['id','descending']).to_a
   end
 
-  def save tweet
-    @mongo.save tweet
+  def mark_thumbs_up tweet
+    mark 'up', tweet
   end
 
+  def mark_thumbs_down tweet
+    mark 'down', tweet
+  end
+
+  def mark_neutral tweet
+    mark 'neutral', tweet
+  end
+  
   def stats 
     # todo use group by, too lazy...
     {
-      :num_unread => @mongo.find({ :read => false }).count,
-      :num_read   => @mongo.find({ :read => true }).count,
+      :thumbs => { 
+        :up        => @mongo.find({ :thumbs => 'up' }).count,
+        :neutral   => @mongo.find({ :thumbs => 'neutral' }).count,
+        :down      => @mongo.find({ :thumbs => 'down' }).count,
+        :undecided => @mongo.find({ :thumbs => { "$exists" => false }}).count
+      }
     }
   end
 
   private
+
+  def mark mark, tweet
+    tweet['thumbs'] = mark
+    @mongo.save tweet
+  end
 
   def get_tweets opts
     raise "need :uid in opts" unless opts[:uid]
