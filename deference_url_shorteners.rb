81299cdd7249d74554924d2587268d230c4827c8
mat <matthew.kelcey@gmail.com>
2010-11-14 13:51:40 +1100

    a simple little url shortener dereferencing util so domain of target links can be shown in tweets

---
+++ deference_url_shorteners.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'redis'
+require 'redis_dbs'
+require 'curb'
+
+class DeferenceUrlShorteners
+
+  SHORTENERS = %(bit.ly goo.gl t.co j.mp is.gd su.pr slidesha.re qr.ae emc.im)
+  SHORT_TO_LONG = 'short_to_long'
+
+  def initialize
+    @r = Redis.new
+    @r.select DEREFERENCE_URL_SHORTENERS
+  end
+
+  def follow_shortening_redirects url 
+    return nil if url.nil? # probably a redirect to 404
+    return url unless SHORTENERS.include? domain_of(url)
+   
+    target = check_cache(url)
+    return target if target
+
+    target = target_of_redirect(url)
+    target = follow_shortening_redirects target
+    cache_redirect url, target
+    target
+
+  end
+
+  def domain_of url
+    return nil if url.nil?
+    url.sub(%r{^.*?//}, '').sub(%r{/.*},'')
+  end
+
+  private
+
+  def check_cache url
+    @r.hget SHORT_TO_LONG, url
+  end
+
+  def cache_redirect url, target
+    @r.hset SHORT_TO_LONG, url, target
+  end
+
+  def target_of_redirect url
+    begin
+      c = Curl::Easy.new url
+      c.http_head
+      redirect = c.header_str.match(/Location: (.*)/)
+      return nil if redirect.nil?
+      redirect[1].chomp
+    rescue
+      nil
+    end
+  end  
+  
+end
+
+=begin
+dus = DeferenceUrlShorteners.new
+STDIN.each do |url|
+  url.chomp!
+  target = dus.follow_shortening_redirects(url)
+  domain = dus.domain_of target
+  if url==target
+    puts "untouched domain [#{domain}] url=[#{url}]"
+  else
+    puts "redirectd domain [#{domain}] url=[#{url}] target=[#{target}]"
+  end
+end
+=end
--- redis_dbs.rb
+++ redis_dbs.rb
@@ -3,9 +3,11 @@ CRAWL_QUEUE_DB = 0
 TWEET_DB = 1
 POSITIVE_FRIENDS_DB = 2
 NEGATIVE_FRIENDS_DB = 3
+DEREFERENCE_URL_SHORTENERS = 4
 
 UID_TO_NAME = 'uid_to_name'
 CRAWL_QUEUE = 'crawl_queue'
 FOLLOWING = 'following'
 FRIENDS_COUNT = 'friends_count'
+
 
