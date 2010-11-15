de6b1546c0407a6916353100f463fdafcba8006b
mat kelcey <matthew.kelcey@gmail.com>
2010-11-15 17:43:21 +1100

    convert word_occ to redis

--- word_occ.rb
+++ word_occ.rb
@@ -1,44 +1,59 @@
 #!/usr/bin/env ruby
-require 'set'
+require 'rubygems'
+require 'redis'
+require 'redis_dbs'
 
 class WordOcc
 
+  POS_COUNTS = 'pos'
+  NEG_COUNTS = 'neg'
+
   def initialize
-    @pos_counts = Hash.new(0)
-    @neg_counts = Hash.new(0)
-    @uniq_tokens = Set.new
+    @r = Redis.new
+    @r.select THUMBS_DB
+    @r.flushdb
   end
 
   def add_training_egs_from file
     File.open(file).each do |line|
       tokens = line.chomp.split
       pos_eg = (tokens.shift == '1')
-      counts = pos_eg ? @pos_counts : @neg_counts
+      counts = pos_eg ? POS_COUNTS : NEG_COUNTS
       tokens.each do |token| 
-        counts[token] += 1
-        @uniq_tokens << token
+        @r.hincrby counts, token, 1
       end
     end
   end
 
-  def info
-    # be wary of items that have only +ve or only -ve
-    @uniq_tokens.each do |token|
-      p = @pos_counts[token]
-      n = @neg_counts[token]
-      score = (p-n).to_f / (p+n)
-      puts [p, n, p-n, p+n, sprintf("%3.3f",score), token].join("\t")
+  def test_from file
+    File.open(file).each do |line|
+      line.chomp!
+      puts "line #{line}"
+      overall_score = 0.0
+      num = 0
+      puts ['p', 'n', 'p-n', 'p+n', 'sc', 'tok'].join("\t")
+      line.split.each do |token|
+        p = @r.hget(POS_COUNTS, token).to_i
+        n = @r.hget(NEG_COUNTS, token).to_i
+        next if p==0 && n==0 # term never seen before
+        score = (p-n).to_f / (p+n)
+        overall_score += score
+        num += 1
+        puts [p, n, p-n, p+n, sprintf("%3.3f",score), token].join("\t")
+      end
+      puts "score #{num==0?:'?':(overall_score/num)}"
     end
   end
 
   def dump
-    puts @uniq_tokens.inspect
-    puts @pos_counts.inspect
-    puts @neg_counts.inspect
+    pk,pv = @r.keys(POS_COUNTS), @r.vals(POS_COUNTS)
+    puts pk.zip(pv).inspect
+    nk,nv = @r.keys(NEG_COUNTS), @r.vals(NEG_COUNTS)
+    puts nk.zip(nv).inspect
   end
   
 end
 
 wo = WordOcc.new
 wo.add_training_egs_from('training')
-wo.info
+wo.test_from('test')
