02326fc97bc9daedebfb49e9e073670bebbabec1
mat <mat@eeepc2.(none)>
2011-09-14 17:27:57 -0700

    calculate bigram likelihood using bigram freq

--- bigram_mutual_info.py
+++ bigram_mutual_info.py
@@ -3,13 +3,15 @@ import sys
 from math import *
 from collections import *
 
-MIN_BIGRAM_SUPPORT = 2
-MIN_UNIGRAM_SUPPORT = 5
+MIN_BIGRAM_SUPPORT = 1#2
+MIN_UNIGRAM_SUPPORT = 1#5
 
-total_unigrams = 0
+unigram_count = 0
+bigram_count = 0
 unigram_freq = defaultdict(int)
 bigram_freq = defaultdict(int)
 
+# extract frequencies
 for line in sys.stdin:
     tokens = line.strip().split()
     tokens.pop(0) # drop id
@@ -19,16 +21,21 @@ for line in sys.stdin:
 
     t1 = tokens.pop(0)
     unigram_freq[t1] += 1
-    total_unigrams += 1
+    unigram_count += 1
 
     while len(tokens)>0:
         t2 = tokens.pop(0)
         unigram_freq[t2] += 1
-        total_unigrams += 1
+        unigram_count += 1
         bigram = (t1,t2)
         bigram_freq[bigram] += 1
+        bigram_count += 1
         t1 = t2
 
+#print "unigram_count", unigram_count
+#print "bigram_count", bigram_count
+
+# emit mutual info
 for bigram in bigram_freq:
     bigram_f = bigram_freq[bigram]
     if bigram_f < MIN_BIGRAM_SUPPORT:
@@ -40,12 +47,12 @@ for bigram in bigram_freq:
     if t1_f < MIN_UNIGRAM_SUPPORT or t2_f < MIN_UNIGRAM_SUPPORT:
         continue
 
-    p_bigram = float(bigram_f) / total_unigrams
-    p_t1 = float(t1_f) / total_unigrams
-    p_t2 = float(t2_f) / total_unigrams
+    p_bigram = float(bigram_f) / bigram_count
+    p_t1 = float(t1_f) / unigram_count
+    p_t2 = float(t2_f) / unigram_count
 
-    mutual_info = log(p_bigram,2) - log(p_t1,2) + log(p_t2,2)
+    mutual_info = p_bigram / (p_t1 * p_t2)
 
-    print mutual_info, t1, t2
+    print mutual_info, bigram_f, p_bigram, t1_f, p_t1, t2_f, p_t2, t1, t2
 
         
